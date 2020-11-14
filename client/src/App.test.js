d4783f5b198ed4ba84be5cf549d13c69c2d3f14a
Andrew <andrewzhu2012@gmail.com>
2020-11-14 18:34:56 +1100

    add shallow test for front end

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App.container';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow((<App />));
+  });
+
+  it('renders children when passed in', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders form', () => {
+    expect(wrapper.find('form').length).toBe(1);
+  });
+
+  it('renders label', () => {
+    expect(wrapper.find('label').length).toBe(5);
+  });
+
+  it('renders input', () => {
+    expect(wrapper.find('input').length).toBe(5);
+  });
+
+  it('renders text', () => {
+    expect(wrapper.text()).toBe('Enter a date to retrive dataMonthOctNovDecDayPick a date');
+  });
 });
